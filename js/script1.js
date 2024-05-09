544c23fff2ecad6d9be20725806c8b5774e60bc0
Cecilia <106674763+emma-cecilia@users.noreply.github.com>
2024-05-09 17:23:47 +0200

    Add files via upload

---
+++ js/script1.js
@@ -0,0 +1,5 @@
+// Optionnel : on peut Changer le texte à faire défiler
+var texte = "Technicienne informatique système réseau et télécom";
+
+var texteDefilant = document.getElementById("texteDefilant");
+texteDefilant.innerText = texte;
